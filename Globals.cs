79b0532ee1484144dc40b58777711964a85c373d
Brody <mondomontage@gmail.com>
2023-10-19 23:33:01 +1100

    Add project files.

---
+++ Globals.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    class Globals
+    {
+        // PUBLIC GLOBAL VARIABLES THAT CAN BE ACCESSED FROM ANYWHERE
+        public static SpriteBatch spriteBatch;
+        public static int WIDTH = 640, HEIGHT = 480;
+        public static Texture2D pixel;
+        public static int player1_score, player2_score;
+    }
+}
---
+++ Paddle.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Paddle {
+        public Rectangle rect;
+        float moveSpeed = 500f;
+        bool isSecondPlayer = false;
+
+        public Paddle(bool isSecondPlayer) {
+            this.isSecondPlayer = isSecondPlayer;
+            rect = new Rectangle((this.isSecondPlayer ? Globals.WIDTH - 20 : 0), 140, 20, 200);
+        }
+        public void Update(GameTime gameTime) {
+
+            // gets information about what key is being pressed
+            KeyboardState kstate = Keyboard.GetState();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            if ((isSecondPlayer ? kstate.IsKeyDown(Keys.Up) : kstate.IsKeyDown(Keys.W)) && rect.Y > 0)
+            {
+                rect.Y -= (int)(moveSpeed * deltaTime);
+            }
+            if ((isSecondPlayer ? kstate.IsKeyDown(Keys.Down) : kstate.IsKeyDown(Keys.S)) && rect.Y < Globals.HEIGHT - rect.Height)
+            {
+                rect.Y += (int)(moveSpeed * deltaTime);
+            }
+
+        }
+        public void Draw() {
+            Globals.spriteBatch.Draw(Globals.pixel, rect, Color.White);
+        }
+    }
+}
