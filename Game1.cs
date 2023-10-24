dc7ccfa581f05db050160e1c1b2bd889808442f0
Dayskie <mondomontage@gmail.com>
2023-10-24 19:19:30 +1100

    Added "gameobject" class, components can inherit from, makes changes easier

--- Game1.cs
+++ Game1.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Pong
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        Paddle paddle, paddle2;
-        Ball ball;
         SpriteFont font;
-        Globals.GAME_STATE state;
+
+        private List<GameObject> _gameObjects;
 
         public Game1()
         {
@@ -24,12 +24,9 @@ namespace Pong
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            paddle = new Paddle(false);
-            paddle2 = new Paddle(true);
-            ball = new Ball();
+
             base.Initialize();
 
-            state = Globals.GAME_STATE.MENU;
         }
 
         protected override void LoadContent()
@@ -39,6 +36,23 @@ namespace Pong
             // TODO: use this.Content to load your game content here
             Globals.pixel = new Texture2D(GraphicsDevice, 1, 1);
             Globals.pixel.SetData(new Color[] { Color.White });
+
+            var texture = Globals.pixel;
+
+            _gameObjects = new List<GameObject>()
+            {
+                new GameObject(texture) {
+                    position = new Vector2(100, 100),
+                    input = new Input() {
+                        Up = Keys.W,
+                        Down = Keys.S,
+                        Left = Keys.A,
+                        Right = Keys.D 
+                    }
+                }
+             };
+
+            
 
             font = Content.LoadLocalized<SpriteFont>("Score");
         }
@@ -50,27 +64,10 @@ namespace Pong
 
             // TODO: Add your update logic here
 
-            switch (state)
+            foreach (var gameObject in _gameObjects)
             {
-                case Globals.GAME_STATE.MENU:
-                    {
-                        if(Keyboard.GetState().IsKeyDown(Keys.E)) { state = Globals.GAME_STATE.GAME; }
-                        break;
-                    }
-                case Globals.GAME_STATE.GAME:
-                    {
-                        paddle.Update(gameTime);
-                        paddle2.Update(gameTime);
-                        ball.Update(gameTime, paddle, paddle2);
-                        break;
-                    }
-                case Globals.GAME_STATE.OVER:
-                    {
-                        if (Keyboard.GetState().IsKeyDown(Keys.E)) { state = Globals.GAME_STATE.GAME; }
-                        break;
-                    }
+                gameObject.Update(gameTime);
             }
-
 
 
             base.Update(gameTime);
@@ -79,34 +76,11 @@ namespace Pong
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-
             Globals.spriteBatch.Begin();
-            // add drawing
-
-            switch (state)
+            foreach(var gameObject in _gameObjects)
             {
-                case Globals.GAME_STATE.MENU:
-                    {
-                        Globals.spriteBatch.DrawString(font, "Main Menu XD\n   Press 'E' To Start", new Vector2(Globals.WIDTH / 2 - 50, Globals.HEIGHT / 2 - 50), Color.White);
-                        break;
-                    }
-                case Globals.GAME_STATE.GAME:
-                    {
-                        paddle.Draw();
-                        paddle2.Draw();
-                        ball.Draw();
-
-                        Globals.spriteBatch.DrawString(font, Globals.player1_score.ToString(), new Vector2(100, 50), Color.White);
-                        Globals.spriteBatch.DrawString(font, Globals.player2_score.ToString(), new Vector2(Globals.WIDTH - 112, 50), Color.White);
-                        break;
-                    }
-                case Globals.GAME_STATE.OVER:
-                    {
-                        Globals.spriteBatch.DrawString(font, "lol", new Vector2(Globals.WIDTH / 2 - 50, Globals.HEIGHT / 2 - 50), Color.White);
-                        break;
-                    }
+                gameObject.Draw(Globals.spriteBatch);
             }
-
             Globals.spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ GameObject.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Pong
+{
+    public class GameObject
+    {
+        protected Texture2D _texture;
+
+        public Vector2 position;
+        public Color color = Color.White;
+        public float speed = 100f;
+        public Input input;
+
+        public GameObject(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            Move(gametime);
+        }
+
+        private void Move(GameTime gametime)
+        {
+            if (input == null) { return; }
+
+            float deltaTime = (float)gametime.ElapsedGameTime.TotalSeconds;
+
+            if (Keyboard.GetState().IsKeyDown(input.Left))
+            {
+                position.X -= speed * deltaTime;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(input.Right))
+            {
+                position.X += speed * deltaTime;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(input.Up))
+            {
+                position.Y -= speed * deltaTime;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(input.Down))
+            {
+                position.Y += speed * deltaTime;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, position, color);
+        }
+
+
+    }
+}
